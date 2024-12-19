e85ddea7326bf1375453f14a90f0fa1d5864354e
Shubh Mehta <shubh.bs21dmu039@spjain.org>
2024-12-20 02:53:43 +0530

    updated vercel.json python and vite.config.js

--- clerk-javascript/vite.config.js
+++ clerk-javascript/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory
@@ -17,7 +18,7 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
-        '@': '/src',
+        '@': path.resolve(__dirname, './src'),
       },
     },
     base: '/',
