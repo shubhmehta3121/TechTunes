b26ddd383aaf8f61b0f5eb24aeec04ca592300eb
Shubh Mehta <shubh.bs21dmu039@spjain.org>
2024-12-20 01:17:33 +0530

    Second Commit

--- clerk-javascript/vite.config.js
+++ clerk-javascript/vite.config.js
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    target: 'esnext',
-  },
-  base: '/',
-  define: {
-    'process.env.VITE_CLERK_PUBLISHABLE_KEY': JSON.stringify(process.env.VITE_CLERK_PUBLISHABLE_KEY)
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [vue()],
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      target: 'esnext',
+    },
+    base: '/',
+    define: {
+      // Stringify environment variables
+      'process.env.VITE_CLERK_PUBLISHABLE_KEY': JSON.stringify(env.VITE_CLERK_PUBLISHABLE_KEY || ''),
+      // Add any other environment variables you need here
+    }
   }
 }) 
