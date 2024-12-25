ffff2c4c05988a77dac2d8bba6b28db147089805
Shubh Mehta <shubh.bs21dmu039@spjain.org>
2024-12-25 23:21:57 +0530

    made more changes

--- clerk-javascript/vite.config.js
+++ clerk-javascript/vite.config.js
@@ -1,22 +1,14 @@
 import { defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    plugins: [vue()],
     build: {
       outDir: 'dist',
       assetsDir: 'assets',
       target: 'esnext',
-      rollupOptions: {
-        output: {
-          manualChunks: undefined
-        }
-      }
     },
     server: {
       port: 3000,
@@ -40,7 +32,7 @@ export default defineConfig(({ mode }) => {
         '@': path.resolve(__dirname, './src'),
       },
     },
-    base: './',
+    base: '/',
     define: {
       'process.env.VITE_CLERK_PUBLISHABLE_KEY': JSON.stringify(env.VITE_CLERK_PUBLISHABLE_KEY || ''),
     }
